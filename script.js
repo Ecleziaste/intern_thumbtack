bf6ccfff41941577839d72f0314b579a2d5d983f
Andrew <65909640+Ecleziaste@users.noreply.github.com>
2020-08-30 20:45:05 +0600

    added forms to registration page

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 "use strict";
 
+var options = {
+    // root: document.querySelector('#scrollArea'),
+    root: document.querySelector('.schools__header'),
+    rootMargin: '0px',
+    threshold: 1.5
+}
+var callback = function (entries, observer) {
+    /* Content excerpted, show below */
+};
+var observer = new IntersectionObserver(callback, options);
